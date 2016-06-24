74212da539c4ba4792c8c56e021732a82bb0ef49
Fabian Lehmann <fabian.lehmann@proclane.com>
2016-06-24 11:36:54 +0200

    added GUI class

---
+++ src/de/m4dhouse/calculator/Fraction/FractionGui.java
@@ -0,0 +1,24 @@
+package de.m4dhouse.calculator.Fraction;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * project     fraction-calculator
+ * package     de.m4dhouse.calculator.Fraction
+ * copyright   (C) Fabian Lehmann 2016
+ * version     1.0.0
+ * author      Fabian Lehmann <fabian.lehmann@gmail.com>
+ */
+public class FractionGui {
+
+    public FractionGui() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(800, 600));
+        frame.setTitle("Fraction Calculator");
+
+        frame.setVisible(true);
+
+    }
+}
