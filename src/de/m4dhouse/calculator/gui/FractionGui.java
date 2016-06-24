a9c5613067fa21aecf4bc1d5f3342225c0a36295
Fabian Lehmann <fabian.lehmann@proclane.com>
2016-06-24 13:00:39 +0200

    Renamed pakages

--- src/de/m4dhouse/calculator/gui/FractionGui.java
+++ src/de/m4dhouse/calculator/gui/FractionGui.java
@@ -11,17 +11,36 @@ import java.awt.*;
  */
 public class FractionGui {
 
+    public static void main(String[] args){
+        new FractionGui();
+
+    }
+
+
     public FractionGui() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(800, 600));
         frame.setTitle("Fraction Calculator");
 
-        JPanel panel = new JPanel();
-
-        panel.setVisible(true);
-
         frame.setVisible(true);
 
+    }
+
+    private JPanel createInputPanel(){
+        JPanel p1 = new JPanel();
+        p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
+
+        JTextField tf1 = new JTextField();
+        JTextField tf2 = new JTextField();
+        tf1.setSize(new Dimension(10,30));
+        tf2.setSize(new Dimension(10,30));
+
+        JLabel l1 = new JLabel("--");
+
+        p1.add(tf1);
+        p1.add(l1);
+        p1.add(tf2);
+        return p1;
     }
 }
