6f0eaac7a2236eeb7f7c10249bf043ad2ca4e314
Fabian Lehmann <fabian.lehmann@proclane.com>
2016-07-04 12:47:29 +0200

    GUI überarbeitet

--- src/de/m4dhouse/calculator/gui/CalculatorGui.java
+++ src/de/m4dhouse/calculator/gui/CalculatorGui.java
@@ -1,6 +1,7 @@
 package de.m4dhouse.calculator.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * project     fraction-calculator
@@ -9,27 +10,27 @@ import javax.swing.*;
  * version     1.0.0
  * author      Fabian Lehmann <fabian.lehmann@gmail.com>
  */
-public class test {
-    private JPanel panel1;
-    private JTextField textField1;
+public class CalculatorGui {
+    private JButton button1;
     private JTextField textField2;
+    private JComboBox comboBox1;
     private JTextField textField3;
     private JTextField textField4;
-    private JButton calculateButton;
-    private JLabel errors;
-    private JComboBox comboBox1;
+    private JPanel panel1;
 
-    private void createUIComponents() {
-
-        // TODO: place custom component creation code here
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("test");
-        frame.setContentPane(new test().panel1);
+    public static void main(String[] args ){
+        JFrame frame = new JFrame();
+        frame.setContentPane(new CalculatorGui().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-
         frame.setVisible(true);
     }
+
+    private String getSelectedOp(){
+        String op = "";
+
+
+        return op;
+    }
+
 }
