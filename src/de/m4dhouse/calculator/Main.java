07ed027f738c7302c32e34df311f41afbaace48b
Hendrik Rombach <h.rombach1@gmail.com>
2016-07-18 10:31:08 +0200

    load gui from cli

--- src/de/m4dhouse/calculator/Main.java
+++ src/de/m4dhouse/calculator/Main.java
@@ -1,8 +1,8 @@
 package de.m4dhouse.calculator;
 
 import de.m4dhouse.calculator.fraction.Fraction;
+import de.m4dhouse.calculator.gui.CalculatorGui;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
@@ -10,55 +10,55 @@ public class Main {
     public static void main(String[] args) {
         boolean guimode = false;
         for (String arg : args) {
-            if (arg.equals("guimode")) {
+            if (arg.equals("--gui") || arg.equals("-g")) {
                 guimode = true;
             }
         }
         if (guimode) {
-            JOptionPane.showMessageDialog(
-                    new JFrame(),
-                    "GUI Mode is not yet ready.",
-                    "Error",
-                    JOptionPane.ERROR_MESSAGE
-                                         );
-            System.exit(0);
-        }
-        try {
-            Scanner sc = new Scanner(System.in);
-            int x1 = 0, x2 = 0 , y1 = 0, y2 = 0;
+            CalculatorGui.main(args);
+        } else {
+            try {
+                Scanner sc = new Scanner(System.in);
+                int     x1 = 0, x2 = 0, y1 = 0, y2 = 0;
 
-            System.out.println("Wählen Sie den ersten Zähler und Nenner");
-            System.out.print("Zähler 1: "); if (sc.hasNextInt()){x1 = sc.nextInt();}
-            System.out.print("Nenner 1: "); if (sc.hasNextInt()){y1 = sc.nextInt();}
-            System.out.println();
+                System.out.println("Wählen Sie den ersten Zähler und Nenner");
+                System.out.print("Zähler 1: ");
+                if (sc.hasNextInt()) {x1 = sc.nextInt();}
+                System.out.print("Nenner 1: ");
+                if (sc.hasNextInt()) {y1 = sc.nextInt();}
+                System.out.println();
 
-            System.out.println("Wählen Sie den zweiten Zähler und Nenner");
-            System.out.print("Zähler 2: "); if (sc.hasNextInt()){x2 = sc.nextInt();}
-            System.out.print("Nenner 2: "); if (sc.hasNextInt()){y2 = sc.nextInt();}
+                System.out.println("Wählen Sie den zweiten Zähler und Nenner");
+                System.out.print("Zähler 2: ");
+                if (sc.hasNextInt()) {x2 = sc.nextInt();}
+                System.out.print("Nenner 2: ");
+                if (sc.hasNextInt()) {y2 = sc.nextInt();}
 
-            Fraction fraction1 = new Fraction(x1, y1);
-            Fraction fraction2 = new Fraction(x2, y2);
+                Fraction fraction1 = new Fraction(x1, y1);
+                Fraction fraction2 = new Fraction(x2, y2);
 
-            System.out.println("welche Operation möchten sie durchführen?");
+                System.out.println("welche Operation möchten sie durchführen?");
 
 
-            String op = "";
+                String op = "";
 
-            System.out.println("| + | - | * | / |");
-            String currOp = "";
-            op = sc.next();
-            Fraction y = new Fraction();
+                System.out.println("| + | - | * | / |");
+                String currOp = "";
+                op = sc.next();
+                Fraction y = new Fraction();
 
 
-            y = op.equals("+") ?
-                fraction1.add(fraction2) : op.equals("-") ?
-                fraction1.sub(fraction2) : op.equals("*") ?
-                fraction1.mul(fraction2) : fraction1.div(fraction2);
+                y = op.equals("+") ?
+                    fraction1.add(fraction2) : op.equals("-") ?
+                                               fraction1.sub(fraction2) : op.equals("*") ?
+                                                                          fraction1.mul(fraction2) : fraction1.div(
+                        fraction2);
 
-            System.out.println(fraction1 + " " + op + " " + fraction2 + " = " + y);
+                System.out.println(fraction1 + " " + op + " " + fraction2 + " = " + y);
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
